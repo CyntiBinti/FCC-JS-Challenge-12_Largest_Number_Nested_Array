208c079749a8cf5daa11de46a0b1ed35db3726c2
Cynthia Mbulu <72040367+CyntiBinti@users.noreply.github.com>
2021-07-02 08:22:05 +0100

    tweak 1

--- file.js
+++ file.js
@@ -1,12 +1,12 @@
 function largestOfFour(arr) {
-   let largest = [];
+   let results = [];
    for (let i = 0; i < arr.length; i++) {
-     for (let j = 0; j < arr[j]; j++) {
-       var largestNumber = arr.reduce(arr[i]);
-        return largest.push(largestNumber);
+     for (let j = 0; j < arr[j].length; j++) {
+       let largestNumber = math.max(...arr[i][j]);
+        return results.push(largestNumber);
      }
    }
-    return largest;
+    return results;
   }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
